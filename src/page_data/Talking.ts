ab6da69699c49f9d303a1160850ac3c12114a7f9
Long He <71657914+HeLongaa@users.noreply.github.com>
2025-05-23 19:17:36 +0800

    ✅[Update] 添加使用Memos的rss订阅作为说说页面数据获取

--- src/page_data/Talking.ts
+++ src/page_data/Talking.ts
@@ -1,15 +1,20 @@
+// page_data/Talking.ts
 export default {
-  // API 接口请求优先，数据格式保持和 data 一致
-  api: 'https://ygjpjcpycxqy.ap-northeast-1.clawcloudrun.com/api/talks',
-  // api: 'http://localhost:3000/api/talks',
-  // api 为空则使用 data 静态数据 
-  // 注意：图片请用 vh-img-flex 类包裹
+  // 数据源类型：api | rss | static
+  api_source: "rss", 
+  
+  // 当api_source=api时生效
+  // api: "https://ygjpjcpycxqy.ap-northeast-1.clawcloudrun.com/api/talks",
+  api: "",
+  
+  // 当api_source=rss时生效，使用Memos的RSS订阅
+  rss_url: "https://corsproxy.io/https://s.helong.online/u/HeLong/rss.xml",
+  
+  // 当api_source=static时生效
   data: [
     {
       "date": "2025-05-08 19:36:16",
-      "tags": [
-        "Todo"
-      ],
+      "tags": ["Todo"],
       "content": "."
     }
   ]
