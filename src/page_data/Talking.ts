7d6a657b1ff670b34b707f788406958bf95dce1e
Long He <71657914+HeLongaa@users.noreply.github.com>
2025-05-23 19:32:04 +0800

    ✅[Update]使用Memos的RSS数据源作为说说页面数据，更改代码结构

--- src/page_data/Talking.ts
+++ src/page_data/Talking.ts
@@ -1,9 +1,23 @@
 export default {
+<<<<<<< Updated upstream
   // API 接口请求优先，数据格式保持和 data 一致
   api: 'https://ygjpjcpycxqy.ap-northeast-1.clawcloudrun.com/api/talks',
   // api: 'http://localhost:3000/api/talks',
   // api 为空则使用 data 静态数据 
   // 注意：图片请用 vh-img-flex 类包裹
+=======
+  // 数据源类型：api | rss | static
+  api_source: "rss", 
+  
+  // 当api_source=api时生效
+  // api: "https://ygjpjcpycxqy.ap-northeast-1.clawcloudrun.com/api/talks",
+  api: "",
+  
+  // 当api_source=rss时生效，使用Memos的RSS订阅
+  rss_url: "https://s.helong.online/u/HeLong/rss.xml",
+  cors_url: "https://corsproxy.io/",
+  // 当api_source=static时生效
+>>>>>>> Stashed changes
   data: [
     {
       "date": "2025-05-08 19:36:16",
--- src/scripts/Talking.ts
+++ src/scripts/Talking.ts
@@ -9,9 +9,27 @@ const TalkingInit = async (data: any) => {
   const talkingDOM = document.querySelector('.main-inner-content>.vh-tools-main>main.talking-main')
   if (!talkingDOM) return;
   try {
+<<<<<<< Updated upstream
     let res = data;
     if (typeof data === 'string') {
       res = await $GET(data);
+=======
+    // 获取数据源
+    let finalData = null
+    switch(config.api_source) {
+      case 'api':
+        finalData = await DATA_SOURCE.api(config.api)
+        break
+      case 'rss':
+        const rss = config.cors_url + config.rss_url
+        finalData = await DATA_SOURCE.rss(rss)
+        break
+      case 'static':
+        finalData = DATA_SOURCE.static(config.data)
+        break
+      default:
+        throw new Error('未知数据源类型')
+>>>>>>> Stashed changes
     }
     talkingDOM.innerHTML = res.map((i: any) => `<article><header><img data-vh-lz-src="https://img.helong.online/pictures/2025/05/20/682b7e5c110ae.png" /><p class="info"><span>HeLong</span><time>${fmtDate(i.date)}前</time></p></header><section class="main">${i.content}</section><footer>${i.tags.map((tag: any) => `<span>${tag}</span>`).join('')}</footer></article>`).join('');
     // 图片懒加载
