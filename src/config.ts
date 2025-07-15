51580fa37fe09d72f4631b622c7e15cdd6f882e7
He Long <helong@stu.ynu.edu.cn>
2025-07-15 20:26:35 +0800

    去除不必要的配置

--- src/config.ts
+++ src/config.ts
@@ -163,19 +163,12 @@ export default {
         "content": "在移动端触控交互中，惯性滚动带来的丝滑体验已成为标配，但鼠标滚轮受限于机械结构，滚动时难免产生生硬的段落感。如何让传统滚轮操作也能获得如触控板般的阻尼反馈？Lenis库通过JavaScript模拟惯性算法，成功将”物理惯性”引入网页滚动，本文将解析其实现原理与实战应用。核心技术原理​滚轮事件拦截与目"
       }
     ]
-  },  // 文章内赞赏码
-  Reward: {
-    // 支付宝收款码
-    AliPay: '/assets/images/alipay.webp',
-    // 微信收款码
-    WeChat: '/assets/images/wechat.webp'
   },
+
   // 访问网页 自动推送到搜索引擎
   SeoPush: {
     enable: false,
     serverApi: '',
     paramsName: 'url'
-  },
-  // 页面阻尼滚动速度
-  ScrollSpeed: 550
+  }
 }
