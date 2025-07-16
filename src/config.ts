1b8d17b814301cee4607dd869c30117591f4a773
He Long <helong@stu.ynu.edu.cn>
2025-07-16 21:56:46 +0800

    朋友界面

--- src/config.ts
+++ src/config.ts
@@ -53,8 +53,8 @@ export default {
     { text: 'HomePage', link: 'https://helong.online', icon: 'Nav_user', newWindow: true },
     { text: 'Archive', link: '/archives', icon: 'Nav_archives' },
     { text: 'Dynamic', link: '/talking', icon: 'Nav_talking' },
+    { text: 'Rss', link: '/friends', icon: 'Nav_rss' },
     { text: 'Links', link: '/links', icon: 'Nav_friends' },
-    // { text: 'Message', link: '/message', icon: 'Nav_message' },
     { text: 'About', link: '/about', icon: 'Nav_about' },
   ],
   // 侧边栏个人网站
@@ -154,12 +154,13 @@ export default {
   // Friends 页面配置
   Friends_conf: {
     // API 接口请求优先，数据格式保持和 data 一致
-    api: '',
+    api: 'https://rss-cloudflare.1946815225.workers.dev/api/rss/public',
+    get_auth_avatar: 'https://favicons.fuzqing.workers.dev/api/getFavicon',
     // api 为空则使用 data 静态数据
     data: [
       {
         "title": "Astro 中使用 Lenis 增加鼠标滚动阻尼感",
-        "auther": "韩小韩博客",
+        "author": "韩小韩博客",
         "date": "2025-03-06",
         "link": "https://www.vvhan.com/article/Lenis-in-Astro",
         "content": "在移动端触控交互中，惯性滚动带来的丝滑体验已成为标配，但鼠标滚轮受限于机械结构，滚动时难免产生生硬的段落感。如何让传统滚轮操作也能获得如触控板般的阻尼反馈？Lenis库通过JavaScript模拟惯性算法，成功将”物理惯性”引入网页滚动，本文将解析其实现原理与实战应用。核心技术原理​滚轮事件拦截与目"
