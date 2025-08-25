c9d6501e19832ad866533d5e5aebe829ab7f6842
He Long <helong_001@qq.com>
2025-08-25 17:08:28 +0800

    robot生成

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,6 +1,7 @@
 import type { APIRoute } from 'astro';
 
 const getRobotsTxt = (sitemapURL: URL) => `User-agent: *
+Allow: /
 Disallow: /MineWeb/
 Disallow: /assets/
 Disallow: /*?*
@@ -9,5 +10,9 @@ Sitemap: ${sitemapURL.href}`;
 
 export const GET: APIRoute = ({ site }) => {
   const sitemapURL = new URL('sitemap-index.xml', site);
-  return new Response(getRobotsTxt(sitemapURL));
+  return new Response(getRobotsTxt(sitemapURL), {
+    headers: {
+      'Content-Type': 'text/plain; charset=utf-8',
+    }
+  });
 };
